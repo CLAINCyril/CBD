0a26005918bfae0c2b9e1788d3971e199e2a2834
cyril <claincyril0@gmail.com>
2020-02-24 12:15:12 +0100

    add dto computer & company

---
+++ CDBwebui/src/main/java/DTO/CompanyDTO.java
@@ -0,0 +1,28 @@
+package DTO;
+
+public class CompanyDTO {
+
+	private Long id;
+	private String name;
+	
+	public CompanyDTO(long id) {
+		this.id=id;
+	}
+	public CompanyDTO() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
